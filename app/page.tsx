b487a054e07d82c0faa1522a43f6b2f9169bdeb1
BaskaLemon <erdenebayarbaasandorj89@gmail.com>
2026-06-16 09:33:50 +0800

    schema prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import Image from "next/image";
+import { PageButtons } from "./components/PageButtons";
+import Header from "./components/Header";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <PageButtons />
+    </div>
+  );
 }
